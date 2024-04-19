190fe42333c62c93111c11c1f90acab78a11e80f
Nuwandi-Dulshara <152952037+Nuwandi-Dulshara@users.noreply.github.com>
2024-04-19 09:29:27 +0530

    Create todohomepage.js

---
+++ todohomepage.js
@@ -0,0 +1,7 @@
+document.getElementById('nextButton').addEventListener('click', function() {
+    window.location.href = 'todo.html' ;
+});
+
+document.getElementById('todoBtn').addEventListener('click', function() {
+    window.location.href = 'todo.html';
+  });
